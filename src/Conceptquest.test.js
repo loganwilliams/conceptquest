79f5a6b6f8413e113671ef5a533cdcf4816dfd1a
Logan Williams <logan.williams@alum.mit.edu>
2018-02-13 13:32:28 -0500

    Added Enzyme, began writing unit tests for CardText and Conceptquest objects

--- src/Conceptquest.test.js
+++ src/Conceptquest.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Conceptquest from './Conceptquest';
+import Card from './Card';
+import CardText from './CardText';
+import {shallow} from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Conceptquest />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<Conceptquest />', () => {
+	it('renders without crashing', () => {
+	  const div = document.createElement('div');
+	  ReactDOM.render(<Conceptquest />, div);
+	  ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it('renders one <Card /> component', () => {
+		const wrapper = shallow(<Conceptquest />);
+    	expect(wrapper.find(Card).length).toBe(1);
+	});
+});
+
+describe('<CardText />', () => {
+	it('can render a link', () => {
+		const wrapper = shallow(<CardText text={[{type: "text", value: "not a link"}, {type: "link", value: "a link", callback: ()=>{}}]} keyValue="test" lineClass="" />);
+		expect(wrapper.find('a').length).toBe(1);
+	});
 });
