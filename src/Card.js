b14407e793fb35ad7e677f4d5dc87ee6e8d0d8f5
Logan Williams <logan.williams@alum.mit.edu>
2018-03-07 22:30:03 -0500

    Fixed issue with rendering a <Card /> component before asynchronous ConceptNet request fulfilled.

--- src/Card.js
+++ src/Card.js
@@ -3,23 +3,29 @@ import CardText from './CardText.js';
 
 class Card extends Component {
   render() {
-    return(
-      <div className="Card">
-        {this.props.items.map((i) => {
-          var lineClass = "";
-          if (i.type === "theme") {
-            lineClass = "Card-line Card-noindent Card-theme";
-          } else if (i.type === "indent") {
-            lineClass = "Card-line Card-indent";
-          } else {
-            lineClass = "Card-line Card-noindent";
-          }
+    if (this.props.items) {
+      return(
+        <div className="Card">
+          {this.props.items.map((i) => {
+            var lineClass = "";
+            if (i.type === "theme") {
+              lineClass = "Card-line Card-noindent Card-theme";
+            } else if (i.type === "indent") {
+              lineClass = "Card-line Card-indent";
+            } else if (i.type === "strong") {
+              lineClass = "Card-line Card-strong Card-indent";
+            } else {
+              lineClass = "Card-line Card-noindent";
+            }
 
-          return <CardText key={i.edge} ref={i.edge} keyValue={i.edge} text={i.text} lineClass={lineClass}/>;
+            return <CardText key={i.edge} ref={i.edge} keyValue={i.edge} text={i.text} lineClass={lineClass}/>;
 
-        })}
-      </div>
-    );
+          })}
+        </div>
+      );
+    } else {
+      return <div></div>
+    }
   }
 }
 
